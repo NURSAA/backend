12cac9f92db623c8fdd460e6f96c4403f0b98d80
musialpatryk <musial.patryk10@gmail.com>
2022-08-18 20:35:57 +0200

    Add users fixtures

---
+++ src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UsersFixtures extends Fixture
+{
+    const USER_CONFIG = [
+        ['admin', [User::ROLE_ADMIN]],
+        ['user', [User::ROLE_USER]]
+    ];
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (UsersFixtures::USER_CONFIG as $userConfig) {
+            $admin = $this->createMockUser($userConfig[0], $userConfig[1]);
+            $manager->persist($admin);
+        }
+
+
+        $manager->flush();
+    }
+
+    private function createMockUser(string $name, array $roles): User
+    {
+        $user = (new User())
+            ->setEmail(sprintf('%s@test.test', $name))
+            ->setRoles($roles);
+
+        $adminPassword = $this->passwordHasher->hashPassword($user, 'test');
+        $user->setPassword($adminPassword);
+
+        return $user;
+    }
+}
